2471364bd199fdae73857a663f967ddbe69a6862
AbhijithCSundaran <161324727+adamandeveddbstudio@users.noreply.github.com>
2024-08-30 21:50:44 +0530

    logo 3 updated

---
+++ 300x250-v3/main.js
@@ -0,0 +1,85 @@
+// Banner duration timer start time
+var startTime;
+// Timeline reference
+var tl;
+init();
+// Init tricggered by onLoad in Body tag
+function init() {
+  // Set Banner duration timer
+  startTime = new Date();
+
+  // Set Global Timeline
+  tl = new TimelineMax({ onComplete: endTime });
+  // tl = gsap.timeline({ repeat: -1, repeatDelay: 3 });
+
+  animate();
+}
+
+function animate() {
+  tl.set(["#main_content"], { autoAlpha: 1, rotationZ: 0.1, force3D: true });
+  tl.set(["#logo"], { autoAlpha: 0 });
+  tl.set(["#faceR"], { x: '-85%', autoAlpha: 1 });
+  tl.set(["#left_content"], { x: '50%' });
+  tl.set(["#left_content", "#right_content"], { scale: 1.6, });
+  tl.set(["#h1L"], { x: '100%', autoAlpha: 1 });
+  tl.set(["#h1R"], { x: '-100%', autoAlpha: 1 });
+  tl.set(["#legal"], { y: 53, autoAlpha: 1 });
+  tl.set(['#h2', '#phn'], { x: '100%' })
+
+  tl.addLabel('frame_1', 0.5)
+    .to('#right_content', 0.7, { width: 'calc(50% - 1.5px)', ease: Power2.easeOut }, "frame_1")
+    .to(['#faceR'], 0.7, { x: 0, ease: Power2.easeOut }, "frame_1+=0.1")
+    .to(['#left_content'], 0.7, { x: 0, ease: Power2.easeOut }, "frame_1+=0.3")
+    .to('#legal', 1, { autoAlpha: 1, y: 0, ease: Power2.easeOut }, "frame_1+=1")
+    .to(['#left_content', '#right_content'], 1, { scale: 1, ease: Power2.easeOut }, "frame_1+=1")
+    .to(['#h1L', '#h1R'], 0.5, { x: 0, ease: Power2.easeOut }, "frame_1+=1.5")
+
+
+    .to('#right_content', 0.5, { width: '35%', ease: Power1.easeOut }, "frame_1+=3")
+    .to(['#faceL', '#faceR'], 0.5, { x: 43, ease: Power1.easeOut }, "frame_1+=3")
+    .to('#right_content', 1, { width: '100%', ease: Power2.easeOut }, "frame_1+=3.5")
+    .to(['#faceL', '#faceR'], 1, { x: '-51%', ease: Power2.easeOut }, "frame_1+=3.5")
+
+
+  tl.addLabel('frame_2', 6)
+    .to(['#faceR', '#h1R'], 0.7, { x: '-100%', autoAlpha: 0, ease: Power2.easeOut }, "frame_2")
+    .to(['#h2', '#phn'], 0.7, { x: 0, autoAlpha: 1, ease: Power2.easeOut }, "frame_2")
+
+  tl.addLabel('frame_3', 10)
+    // .to(['#h2', '#phn', '#legal'], 0.7, { x: '-100%', autoAlpha: 0, ease: Power2.easeOut }, "frame_3")
+    // .to(['#h3'], 0.7, { x: 0, autoAlpha: 1, ease: Power1.easeInOut }, "frame_3")
+    .staggerTo(['#h2', '#phn'], 0.5, { autoAlpha: 0, ease: Power2.easeOut }, 0.5, "frame_3")
+    .staggerTo(['#h3', '#logo', '#cta'], 0.5, { autoAlpha: 1, ease: Power2.easeOut }, 0.5, 'frame_3+=1')
+  // .staggerFrom(['#h1 span', '#h2 span'], 1, { y: '+=50', ease: Power2.easeOut }, 0.3, 'frame_1+=2')
+
+}
+
+function endTime() {
+  // show total banner animation time in browser console.
+  var endTime = new Date();
+  setRollover();
+  // console.log( "Animation duration: " + (endTime - startTime) / 1000 + " seconds");
+}
+
+// CTA grow on hover
+
+function setRollover() {
+  var clickArea = document.getElementById("default_exit");
+  clickArea.onclick = function () {
+    window.open(clickTag, "_blank");
+  };
+  // clickArea.addEventListener("mouseover", default_over, false);
+  // clickArea.addEventListener("mouseout", default_out, false);
+}
+
+// function default_over(event) {
+//   // TweenMax.to(["#cta span"], 0.3, { scale: 1.1, ease: Power1.easeOut, delay: 0, backgroundColor: "#014732", color: "#fff" });
+//   // TweenMax.to(["#cta span svg"], 0.3, { fill: "#fff" }, "<");
+// }
+
+// function default_out(event) {
+//   // TweenMax.to(["#cta span"], 0.3, { scale: 1, ease: Power1.easeOut, delay: 0, backgroundColor: "inherit", color: "#000" });
+//   // TweenMax.to(["#cta span svg"], 0.3, { fill: "#000" }, "<");
+// }
+
+
